8681674cb824bb77a7344b7b1ec06ea919243b2e
SharmaAntriksh <73054674+SharmaAntriksh@users.noreply.github.com>
2023-04-13 15:29:33 +0530

    Update Get Users.py

--- Python/Get Users.py
+++ Python/Get Users.py
@@ -3,12 +3,19 @@ import requests
 import pandas as pd
 import itertools
 
+# pip install msrest, mstrestazure
+# pip install azure.graphrbac
+# pip install azure.common
+
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.graphrbac import GraphRbacManagementClient
+
+app_id = '*********************************************'
+pbi_tenant_id = '*********************************************'
+app_secret = '*********************************************'
+
 
 def get_access_token():
-    
-    app_id = '*****************************************'
-    pbi_tenant_id = ''*****************************************''
-    app_secret = ''*****************************************''
     
     authority_url = f'https://login.microsoftonline.com/{pbi_tenant_id}'
     scopes = [r'https://analysis.windows.net/powerbi/api/.default']
@@ -19,8 +26,8 @@ def get_access_token():
     token = response.get('access_token')
     
     return token
-    
-    
+
+
 token = get_access_token()
 headers = {
     'Content-Type': 'application/json',
@@ -31,37 +38,70 @@ headers = {
 def get_workspaces():
     
     # workspace_endpoint = 'https://api.powerbi.com/v1.0/myorg/groups?$filter=(isOnDedicatedCapacity eq true)'
-    workspace_endpoint = 'https://api.powerbi.com/v1.0/myorg/groups'
-    response_request = requests.get(workspace_endpoint, headers=headers)
+    
+    workspaces = 'https://api.powerbi.com/v1.0/myorg/groups'
+    response_request = requests.get(workspaces, headers=headers)
     result = response_request.json()
     
     workspace_id = [workspace['id'] for workspace in result['value']]
     workspace_name = [workspace['name'] for workspace in result['value']]
     
     return zip(workspace_id, workspace_name)
+
+
+def get_group_members(group_id):
     
+    # Code taken from https://stackoverflow.com/questions/51859504/how-to-access-the-azure-ad-groups-and-user-details-using-python
     
+    credentials = ServicePrincipalCredentials(
+        client_id = app_id,
+        secret = app_secret,
+        resource = "https://graph.windows.net",
+        tenant = '16ky80.onmicrosoft.com' # Change 16ky80 to your tenant
+    )
+
+    graphrbac_client = GraphRbacManagementClient(
+        credentials,
+        pbi_tenant_id
+    )
+    
+    users = graphrbac_client.groups.get_group_members(group_id)
+    
+    result = ''
+    
+    for u in users:
+        if u.object_type == 'User':
+            result += u.mail  + '\n'
+        elif u.object_type == 'ServicePrincipal':
+             result +=  'Service Principal - ' + u.display_name + '\n'
+
+    return result[:-1]
+
+
 def get_workspace_users():
     
     workspace_ids, workspace_names = zip(*list(get_workspaces()))
     user_details = []
 
     for workspace_id in workspace_ids:
-        users_endpoint = fr"https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/users"
-        response = requests.get(users_endpoint, headers = headers)
+        users = fr"https://api.powerbi.com/v1.0/myorg/groups/{workspace_id}/users"
+        response = requests.get(users, headers = headers)
         response_text = response.json()['value']
+        
         for entry in response_text:
             entry['WorkspaceID'] = workspace_id
+            if entry['principalType'] == 'Group':
+                entry['Group Membership'] = get_group_members(entry['identifier'])
         
         user_details.append(response_text)
         
     return list(itertools.chain(*user_details))
-    
-    
+
+
 workspace_df = pd.DataFrame.from_records(get_workspaces(), columns = ['WorkspaceID', 'WorkspaceName'])
 workspace_user_df = pd.DataFrame(get_workspace_users())
 
-final_df = pd.merge(workspace_df, workspace_user_df, how = 'inner', on = 'WorkspaceID' )
+final_df = pd.merge(workspace_df, workspace_user_df, how = 'left', on = 'WorkspaceID' )
 final_df.rename(
     columns = {
         'WorkspaceID': 'Workspace ID',
@@ -74,6 +114,4 @@ final_df.rename(
     },
     inplace = True
 )
-
-final_df['Email ID'] = final_df['Email ID'].fillna('')
 final_df.to_excel(r"C:\Users\antsharma\OneDrive\Desktop\User Details.xlsx", index = False)
