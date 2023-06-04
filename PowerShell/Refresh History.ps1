d99c82aaafca8780251b4c87d9d44bb06090c427
SharmaAntriksh <73054674+SharmaAntriksh@users.noreply.github.com>
2023-06-04 16:49:25 +0530

    Formatting changes + App login

--- PowerShell/Refresh History.ps1
+++ PowerShell/Refresh History.ps1
@@ -1,37 +1,51 @@
-Login-PowerBIServiceAccount
-$Headers = Get-PowerBIAccessToken
+# Install-Module -Name MicrosoftPowerBIMgmt
+
+$AppId = "924418cd-******************************"
+$TenantId = "443c0ee8-***************************"
+$ClientSecret = "cUh8Q~**************************" 
+
+$SecurePassword = ConvertTo-SecureString $ClientSecret -Force -AsPlainText
+$Credential = New-Object Management.Automation.PSCredential($AppId, $SecurePassword)
+
+Connect-PowerBIServiceAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential
+$Headers = Get-PowerBIAccessToken 
 
 $Workspaces = Get-PowerBIWorkspace -Scope Individual | 
-    ` Where-Object { 
-            $_.IsOnDedicatedCapacity -eq 'true' -and $_.Name -notin ('Admin monitoring', 'Fabric Demo') 
+    Where-Object { 
+        $_.IsOnDedicatedCapacity -eq 'true' -and $_.Name -notin ('Admin monitoring', 'Fabric Demo') 
     }
 
 $ResultArray = @()
+$ApiString = "https://api.powerbi.com/v1.0/myorg/"
 
 $Workspaces | ForEach-Object {
+
     $Workspace = $_
+    $Datasets = Invoke-RestMethod `
+                    -Method Get `
+                    -Headers $Headers `
+                    -Uri "$($ApiString)/groups/$($Workspace.Id)/datasets" 
 
-    $Datasets = Invoke-RestMethod -Method Get -Headers $Headers -Uri "https://api.powerbi.com/v1.0/myorg/groups/$($Workspace.Id)/datasets" 
+    $Datasets.Value | ForEach-Object {
 
-    $Datasets.value | ForEach-Object {
         $Dataset = $_
-
-        $Refreshes = Invoke-RestMethod -Method Get -Headers $Headers -Uri "https://api.powerbi.com/v1.0/myorg/groups/$($Workspace.Id)/datasets/$($Dataset.Id)/refreshes"
+        $Refreshes = Invoke-RestMethod `
+                        -Method Get `
+                        -Headers $Headers `
+                        -Uri  "$($ApiString)/groups/$($Workspace.Id)/datasets/$($Dataset.Id)/refreshes"
 
         $Refreshes.Value | ForEach-Object {
-            $Refresh = $_
 
+            $Refresh = $_
             $Row = New-Object PSObject -Property @{
-                WorkspaceID = $Workspace.Id;
-                WorkspaceName = $Workspace.Name;
-                DatasetID = $Dataset.Id;
-                DatasetName = $Dataset.Name;
-                DatasetOwner = $Dataset.configuredBy;
-                DatasetCreatedOn = $Dataset.createdDate;
-                RefreshID = $Refresh.RequestId;
-                RefreshType = $Refresh.refreshType;
+                WorkspaceID      = $Workspace.Id;
+                WorkspaceName    = $Workspace.Name;
+                DatasetID        = $Dataset.Id;
+                DatasetName      = $Dataset.Name;
+                RefreshID        = $Refresh.RequestId;
+                RefreshType      = $Refresh.refreshType;
                 RefreshStartTime = $Refresh.startTime;
-                RefreshEndTime = $Refresh.endTime
+                RefreshEndTime   = $Refresh.endTime
             }
 
             $ResultArray += $Row
@@ -41,5 +55,9 @@ $Workspaces | ForEach-Object {
 }
 
 $ResultArray | 
-    ` Select-Object -Property WorkspaceID, WorkspaceName, DatasetID, DatasetName,DatasetOwner, DatasetCreatedOn, RefreshID, RefreshType, RefreshStartTime, RefreshEndTime |
-    ` Export-Csv -NoTypeInformation -Path "C:\Users\antsharma\Downloads\refresh.csv"
+    Select-Object -Property `
+        WorkspaceID, WorkspaceName, DatasetID, DatasetName, 
+        RefreshID, RefreshType, RefreshStartTime, RefreshEndTime |
+
+    Export-Csv -NoTypeInformation `
+        -Path "$($env:USERPROFILE)\Downloads\refresh.csv"
